c1dadb57924645548de862d382a3e9fc138e4daf
grantchan <chan.grant@gmail.com>
2021-05-14 22:28:07 +0800

    Shading with surface normal

--- main.cc
+++ main.cc
@@ -1,13 +1,18 @@
 #include <iostream>
+
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point& center, double radius, const ray& r) {
+std::ostream& operator<<(std::ostream& out, color rhs) {
+    return out << rhs.x() << ' ' << rhs.y() << ' ' << rhs.z();
+}
+
+double hit_sphere(const point& center, double radius, const ray& r) {
     vec3<double> oc = r.origin() - center;
     double a = r.direction().dot(r.direction());
     double b = 2.0 * oc.dot(r.direction());
     double c = oc.dot(oc) - radius * radius;
-    return (b*b - 4*a*c) > 0;
+    return b*b - 4*a*c;
 }
 
 int main() {
@@ -40,18 +45,20 @@ int main() {
             dir += v*y_offset;
             dir -= origin;
             ray r(origin, dir);
-            if (hit_sphere(point(0, 0, -1), 0.5, r)) {
-                color c(255, 0, 0);
 
-                std::cout << c.x() << ' ' << c.y() << ' ' << c.z() << '\n';
+            double t = hit_sphere(point(0, 0, -1), 0.5, r);
+            if (t >= 0.0) {
+                vec3<double> norm = r.at(t).unit() - vec3<double>(0, 0, -1);
+                norm += 1;
+                norm *= 0.5;
+                norm *= 255.999;
+                std::cout << color((int)norm.x(), (int)norm.y(), (int)norm.z()) << '\n';
             } else {
                 vec3<double> unit_dir = r.direction().unit();
                 double t = 0.5*(unit_dir.y() + 1.0);
                 vec3<double> cc = 255.999 * (vec3<double>(1.0-t, 1.0-t, 1.0-t) + t*vec3<double>(0.5, 0.7, 1.0));
 
-                color c((int)cc.x(), (int)cc.y(), (int)cc.z());
-
-                std::cout << c.x() << ' ' << c.y() << ' ' << c.z() << '\n';  
+                std::cout << color((int)cc.x(), (int)cc.y(), (int)cc.z()) << '\n';  
             }
         }
     }
